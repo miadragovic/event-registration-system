1b752c1ea8d7538c6b60adad4dad1840afc4fb9c
miadragovic <mdragovic.ieu2023@student.ie.edu>
2025-11-26 17:32:35 +0100

    Use SQLite as fallback DB if Azure connection string not set

--- database.py
+++ database.py
@@ -2,8 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.environ["DB_CONN_STRING"]
+SQLALCHEMY_DATABASE_URL = os.environ.get("DB_CONN_STRING", "sqlite:///./test.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
